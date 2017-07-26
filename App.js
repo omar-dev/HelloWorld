40846c048bcdf9ee5f5e26853705f05ca4f041a4
omar-dev <profomarm@gmail.com>
2017-07-26 19:58:28 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import {
+  Button,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View, I18nManager
+} from 'react-native';
+import {
+  createNavigator,
+  createNavigationContainer,
+  TabRouter,
+  addNavigationHelpers,StackNavigator,TabNavigator,TabBarBottom
+} from 'react-navigation';
+
+class MyHomeScreen extends React.Component {
+  static navigationOptions = {
+    tabBarLabel: 'المنزل',
+    // Note: By default the icon is only shown on iOS. Search the showIcon option below.
+    // tabBarIcon: ({ tintColor }) => (
+    //   <Image
+    //     source={require('./chats-icon.png')}
+    //     style={[styles.icon, {tintColor: tintColor}]}
+    //   />
+    // ),
+  };
+
+  render() {
+    return (
+      <Button
+        onPress={() => this.props.navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    );
+  }
+}
+
+class MyNotificationsScreen extends React.Component {
+  static navigationOptions = {
+    tabBarLabel: 'التنبيهات',
+    // tabBarIcon: ({ tintColor }) => (
+    //   <Image
+    //     source={require('./notif-icon.png')}
+    //     style={[styles.icon, {tintColor: tintColor}]}
+    //   />
+    // ),
+  };
+
+  render() {
+    return (
+      <Button
+        onPress={() => this.props.navigation.goBack()}
+        title="Go back home"
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 26,
+    height: 26,
+  },
+});
+
+const MyApp = TabNavigator({
+  Home: {
+    screen: MyHomeScreen,
+  },
+  Notifications: {
+    screen: MyNotificationsScreen,
+  },
+}, {
+  tabBarComponent: TabBarBottom,
+  tabBarPosition: 'bottom',
+  tabBarOptions: {
+    activeTintColor: '#e91e63',
+  },
+});
+
+
+I18nManager.allowRTL(true);
+I18nManager.forceRTL(true);
+
+class App extends React.Component {
+
+  render() {
+    return <MyApp />
+  }
+}
+export default App;
